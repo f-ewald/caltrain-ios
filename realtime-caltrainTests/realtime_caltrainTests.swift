9e9581c056d8faf23c9b1d18fee9873ccdf8a9d9
Friedrich Ewald <fewald@linkedin.com>
2026-01-27 22:34:35 -0800

    Initial Commit

---
+++ realtime-caltrainTests/realtime_caltrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  realtime_caltrainTests.swift
+//  realtime-caltrainTests
+//
+//  Created by Friedrich Ewald on 1/27/26.
+//
+
+import Testing
+@testable import realtime_caltrain
+
+struct realtime_caltrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
